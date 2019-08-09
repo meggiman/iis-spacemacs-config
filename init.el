f5507814701aa9b4ff89399ade54364dfdb41120
Manuel Eggimann <meggimann@iis.ee.ethz.ch>
2019-08-09 15:27:35 +0200

    Map CTRL+Mousewheel to zoom in and out in PDFView-mode

--- init.el
+++ init.el
@@ -678,28 +678,34 @@ you should place your code here."
       (defun org-message-thunderlink-open (slash-message-id)
         "Handler for org-link-set-parameters that converts a standard message:// link into
    a thunderlink and then invokes thunderbird."
-        ;; remove any / at the start of slash-message-id to create real message-id
-        (let ((message-id
-               (replace-regexp-in-string (rx bos (* "/"))
-                                         ""
-                                         slash-message-id)))
-          (start-process
-           (concat "thunderlink: " message-id)
-           nil
-           thunderbird-program
-           "-thunderlink"
-           (concat "thunderlink://messageid=" message-id)
-           )))
-      ;; on message://aoeu link, this will call handler with //aoeu
-      (org-link-set-parameters "message" :follow #'org-message-thunderlink-open))
+      ;; remove any / at the start of slash-message-id to create real message-id
+      (let ((message-id
+             (replace-regexp-in-string (rx bos (* "/"))
+                                       ""
+                                       slash-message-id)))
+        (start-process
+         (concat "thunderlink: " message-id)
+         nil
+         thunderbird-program
+         "-thunderlink"
+         (concat "thunderlink://messageid=" message-id)
+         )))
+    ;; on message://aoeu link, this will call handler with //aoeu
+    (org-link-set-parameters "message" :follow #'org-message-thunderlink-open))
 
 
-    ;; Configure bibtex layer
-    (setq org-ref-default-bibliography '("~/org-ref/library.bib")
-          org-ref-pdf-directory "~/org-ref/pdfs"
-          org-ref-bibliography-notes "~/org_notes/literature-notes.org"
-          org-ref-get-pdf-filename-function 'org-ref-get-mendeley-filename
-          bibtex-completion-pdf-field "file")
+  ;; Configure bibtex layer
+  (setq org-ref-default-bibliography '("~/org-ref/library.bib")
+        org-ref-pdf-directory "~/org-ref/pdfs"
+        org-ref-bibliography-notes "~/org_notes/literature-notes.org"
+        org-ref-get-pdf-filename-function 'org-ref-get-mendeley-filename
+        bibtex-completion-pdf-field "file")
+
+
+  ;; Configure PDFview to zoom with mousewheel
+  (with-eval-after-load 'pdf-view
+    (define-key pdf-view-mode-map (kbd "<C-mouse-5>") (lambda () (interactive) (pdf-view-shrink 1.05)))
+    (define-key pdf-view-mode-map (kbd "<C-mouse-4>") (lambda () (interactive) (pdf-view-enlarge 1.05))))
 
     )
 
