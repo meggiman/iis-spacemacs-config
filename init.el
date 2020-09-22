85f21d2c8707c69d3c4d54d3133f5f5f983ec8e9
Manuel Eggimann <meggimann@iis.ee.ethz.ch>
2020-09-22 14:12:55 +0200

    Modify Centaur-tabs tab grouping function

--- init.el
+++ init.el
@@ -593,6 +593,44 @@ emacs users to use the editor."
 
   (show-iis-emacs-quickstart-buffer)
 
+  ;;Configure Centaur Tabs grouping
+  (defun centaur-tabs-buffer-groups ()
+    "`centaur-tabs-buffer-groups' control buffers' group rules.
+
+    Group centaur-tabs with mode if buffer is derived from `eshell-mode' `emacs-lisp-mode' `dired-mode' `org-mode' `magit-mode'.
+    All buffer name start with * will group to \"Emacs\".
+    Other buffer group by `centaur-tabs-get-group-name' with project name."
+    (list
+     (cond
+      ((or (string-equal "*" (substring (buffer-name) 0 1))
+           (memq major-mode '(magit-process-mode
+                              magit-status-mode
+                              magit-diff-mode
+                              magit-log-mode
+                              magit-file-mode
+                              magit-blob-mode
+                              magit-blame-mode
+                              )))
+       "Emacs")
+      ((derived-mode-p 'dired-mode)
+       "Dired")
+      ((memq major-mode '(helpful-mode
+                          help-mode))
+       "Help")
+      ((memq major-mode '(org-mode
+                          org-agenda-clockreport-mode
+                          org-src-mode
+                          org-agenda-mode
+                          org-beamer-mode
+                          org-indent-mode
+                          org-bullets-mode
+                          org-cdlatex-mode
+                          org-agenda-log-mode
+                          diary-mode))
+       "OrgMode")
+	 (t
+	  (centaur-tabs-get-group-name (current-buffer))))))
+
 
 
   ;; Enable treemacs mode and add hook to buffer switch to auto update treemacs with current projectile root
